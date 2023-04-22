bf63547627369d926e4fb476e104195fbe767891
Jayakumarkjk <115686350+Jayakumarkjk@users.noreply.github.com>
2023-04-22 10:22:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+var h1=document.getElementById("task");         
+h1.innerText="Countdown starts";        
+
+
+addEventListener("load",()=>{            
+    setTimeout(()=>{
+        h1.innerText="10"
+
+    setTimeout(()=>{
+        h1.innerText="9"
+
+    setTimeout(()=>{
+        h1.innerText="8"
+
+    setTimeout(()=>{
+        h1.innerText="7"
+
+    setTimeout(()=>{
+        h1.innerText="6" 
+
+    setTimeout(()=>{
+        h1.innerText="5"
+
+    setTimeout(()=>{
+        h1.innerText="4"
+
+    setTimeout(()=>{
+        h1.innerText="3"
+
+    setTimeout(()=>{
+        h1.innerText="2"
+
+    setTimeout(()=>{
+        h1.innerText="1"
+
+    setTimeout(()=>{
+        h1.innerText="Happy Independence Day!"
+        
+    },2000)        
+    },1000)                                
+    },1000)     
+    },1000)      
+    },1000)  
+    },1000)     
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)                           
+})
